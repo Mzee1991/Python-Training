039f9af00ccad74c71dda14f3bef4f2a276b56e1
Mzee1991 <yohanamariamzee@gmail.com>
2022-06-04 12:40:33 +0300

    asd

--- server.py
+++ server.py
@@ -1,9 +1,10 @@
+#!/usr/bin/python3
 import socket
 
 s = socket.socket()
 print('Socket Created')
 
-s.bind(('192.168.1.1', 9999))
+s.bind(('localhost', 9999))
 
 s.listen(3)
 print('Waiting for connections')
