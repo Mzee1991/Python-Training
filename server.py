1a85aa9758511b9bdc165bfbdd7eaabbb759645e
Mzee1991 <yohanamariamzee@gmail.com>
2022-06-04 10:04:39 +0300

    asd

---
+++ server.py
@@ -0,0 +1,15 @@
+import socket
+
+s = socket.socket()
+print('Socket Created')
+
+s.bind(('localhost', 9999))
+
+s.listen(3)
+print('Waiting for connections')
+
+while True:
+    c, addr = s.accept()
+    print("Connected with ", addr)
+    c.send('Welcome to Kyojo')
+    c.close()
