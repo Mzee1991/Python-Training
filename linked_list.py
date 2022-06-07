92f281931396598e6e8e24a6d7b55b9fed08eea7
Batya Tonny <84370752+devbxtzz@users.noreply.github.com>
2022-06-07 21:20:25 +0300

    Update linked_list.py

--- linked_list.py
+++ linked_list.py
@@ -31,8 +31,8 @@ class linked_list:
         cur_node = self.head
         while cur_node is not None:
             if cur_node != self.head:
-                print("Name: {}\nAge: {}\nSex: {}\n".format(cur_node.name,
-                                                            cur_node.age, cur_node.sex))
+                print("Name: {}\nAge: {}\nSex: {}\n".format(cur_node.name, 
+                        cur_node.age, cur_node.sex))
             cur_node = cur_node.next
 
 
