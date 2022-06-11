9aff6171db0114589b96d8b3497d45832e94340b
Mzee1991 <yohanamariamzee@gmail.com>
2022-06-11 21:18:47 +0300

    well

--- Doubly_linked.py
+++ Doubly_linked.py
@@ -2,8 +2,10 @@
 
 class Node:
 
-    def __init__(self, data):
-        self.data = data
+    def __init__(self, name, age, sex):
+        self.name = name
+        self.age = age
+        self.sex = sex
         self.next = None
         self.prev = None
 
@@ -14,26 +16,26 @@ class DoublyLinkedList:
         self.head = None
 
     # insert node at the front
-    def insert_front(self, data):
+    def insert_front(self, name, age, sex):
 
-        new_node = Node(data)
+        new_node = Node(name, age, sex)
         new_node.next = self.head
         if self.head is not None:
             self.head.prev = new_node
         self.head = new_node
 
-    def insert_after(self, prev_node, data):
+    def insert_after(self, prev_node, name, age, sex):
         if prev_node is None:
             print("previous node cannot be null")
             return
-        new_node = Node(data)
+        new_node = Node(name, age, sex)
         new_node.next = prev_node.next
         prev_node.next = new_node
         new_node.prev = prev_node
         if new_node.next:
             new_node.next.prev = new_node
 
-    def insert_end(self, data):
+    def insert_end(self, name, age, sex):
         new_node = Node(data)
         if self.head is None:
             self.head = new_node
@@ -58,7 +60,7 @@ class DoublyLinkedList:
     def display_list(self, node):
 
         while node:
-            print(node.data, end="->")
+            print("Name: {}\nAge: {}\nSex: {}\n".format(node.name, node.age, node.sex))
             last = node
             node = node.next
 
@@ -66,14 +68,14 @@ class DoublyLinkedList:
 # initialize an empty node
 d_linked_list = DoublyLinkedList()
 
-d_linked_list.insert_end(5)
-d_linked_list.insert_front(1)
-d_linked_list.insert_front(6)
-d_linked_list.insert_end(9)
+d_linked_list.insert_end("Mzee John", 30, "Male")
+d_linked_list.insert_front("Mukasa Joseph", 27, "Male")
+d_linked_list.insert_front("Cecilia Tusingwire", 24, "Female")
+d_linked_list.insert_end("Charles Mwesige", 32, "Male")
 
-d_linked_list.insert_after(d_linked_list.head, 11)
+d_linked_list.insert_after(d_linked_list.head, "Aaron", 20, "Male")
 
-d_linked_list.insert_after(d_linked_list.head.next, 15)
+d_linked_list.insert_after(d_linked_list.head.next, "Karungi", 23, "Female")
 
 d_linked_list.display_list(d_linked_list.head)
 
